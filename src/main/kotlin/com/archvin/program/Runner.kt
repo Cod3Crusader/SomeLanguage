1b3e5e3a2606bf424fb870ad590720db1beb5771
mintvin <k.edvin.szilard@gmail.com>
2026-05-25 19:09:59 +0200

    parameter order fix (temporary)

--- src/main/kotlin/com/archvin/program/Runner.kt
+++ src/main/kotlin/com/archvin/program/Runner.kt
@@ -23,7 +23,8 @@ class Runner : Processor<Unit, Instruction>() {
             is Instruction.CallInstr -> {
                 val func = instr.function
                 if (func is FunctionValue.BuiltinFunction) {
-                    val params = func.paramTypes.indices.map { stack.pop() }
+                    val params = func.paramTypes.indices.map { stack.pop() }.reversed()
+                    // TODO: reverse at compile time
 
                     func.call(params).takeIf { func.returnType !is BuiltinType.VoidType }?.let { stack.push(it) }
                 } else {
