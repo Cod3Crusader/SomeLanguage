f366acdda1ea50e35134439811de01f04c0d580c
CodeCrus3der <mr.cheese.hu@gmail.com>
2026-06-06 20:58:11 +0100

    TypeChecker changes

--- src/main/kotlin/com/archvin/pipeline/finalizing/TypeChecker.kt
+++ src/main/kotlin/com/archvin/pipeline/finalizing/TypeChecker.kt
@@ -1,24 +1,34 @@
 package com.archvin.pipeline.finalizing
 
+
 import com.archvin.data.type.BuiltinType.AnyType
 import com.archvin.data.type.BuiltinType.VoidType
 import com.archvin.data.type.Type
+import com.archvin.data.HasType
+import com.archvin.data.value.Value
 import com.archvin.data.variable.Symbol.Function.CustomFunction
 import com.archvin.data.variable.Symbol.Variable
 import com.archvin.exceptions.CompileError
 import com.archvin.pipeline.Stage
-import com.archvin.pipeline.finalizing.CheckedExpr.*
+import com.archvin.pipeline.finalizing.Instruction.*
+import com.archvin.pipeline.finalizing.Instruction
 import com.archvin.pipeline.parsing.Expression
 
-class TypeChecker : Stage.ConsumerStage<CheckedExpr, Expression>() {
+
+class TypeChecker : Stage<Instruction, Expression>() {
     val resolver = NameResolver()
 
-    fun checkType(expr: Expression, expectType: Type): CheckedExpr {
-        val expr: CheckedExpr = when (expr) {
+
+    fun checkType(expr: Expression, expectType: Type) {
+        val instr: TypedInstruction = when (expr) {
             is Expression.AssignExpr -> {
                 val variable = resolver.resolveVar(expr.id)
 
-                Assign(variable, checkType(expr.assigned, variable.type))
+
+                checkType(expr.assigned, variable.type)
+
+
+                AssignInstr(variable) retType VoidType
             }
             is Expression.DeclareExpr -> {
                 val symbolType = resolver.resolveType(expr.typeId)
@@ -27,29 +37,42 @@ class TypeChecker : Stage.ConsumerStage<CheckedExpr, Expression>() {
                     else Variable(expr.id, symbolType)
                 )
 
-                if (expectType !is AnyType) throw CompileError.TypeMismatchError(expectType, VoidType)
 
-                next()!!
+                TypedInstruction(null, VoidType)
             }
             is Expression.CallExpr -> {
                 val func = resolver.resolveFunc(expr.functionId)
 
-                Call(func, expr.params
-                    .mapIndexed { i, it -> checkType(it, func.type.paramTypes[i]) })
+
+                expr.params.mapIndexed { i, it -> checkType(it, func.type.paramTypes[i]) }
+
+
+                CallInstr(func) retType func.type.retType
             }
             is Expression.LambdaExpr -> {
                 TODO()
                 //Lit(LambdaVal.Composite(expr.expressions.map { checkType(it, AnyType)!! }))
             }
-            is Expression.LitExpr<*> -> Lit(expr.lit)
+            is Expression.LitExpr<*> -> LitInstr(Value.Primitive(expr.lit.value)) retType expr.lit.type
             is Expression.OpExpr -> TODO()
-            is Expression.ReadExpr -> Read(resolver.resolveVar(expr.id))
+            is Expression.ReadExpr -> {
+                val symbol = resolver.resolveVar(expr.id)
+                ReadInstr(symbol) retType symbol.type
+            }
         }
 
-        if (expr.type != expectType) throw CompileError.TypeMismatchError(expectType, expr.type)
 
-        return expr
+        if (instr.type != expectType) throw CompileError.TypeMismatchError(expectType, instr.type)
+
+
+        instr.instr?.let { yield(it) }
     }
 
-    override fun consume(c: Expression) = checkType(c, AnyType)
+
+    override fun step(c: Expression) { checkType(c, AnyType) }
+
+
+    private class TypedInstruction(val instr: Instruction?, override val type: Type) : HasType
+    private infix fun Instruction.retType(type: Type) = TypedInstruction(this, type)
 }
+
